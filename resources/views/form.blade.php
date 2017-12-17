0ebe843213683a9c75ee81a8675d7557c4ba2a88
1ike <googlohren@gmail.com>
2017-12-17 17:04:25 +0300

    Start!

---
+++ resources/views/form.blade.php
@@ -0,0 +1,8 @@
+<form class="search form-inline" method="post">
+
+    <input class="form-control" id="url" name="url" value="{{ $url }}" placeholder="Enter webpage URL" required="" type="text">
+
+    <input class="btn btn-primary" value="Test"  type="submit">
+<!-- <button type="submit" class="btn btn-primary">Confirm identity</button> -->
+</form>
+
